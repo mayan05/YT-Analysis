02cd147a65270252bf90c7fc83b3d61e88bea09e
Mayan Sequeira <mayan.sequeira@gmail.com>
2025-03-19 23:02:17 +0530

    Added Header

---
+++ mysite/src/Components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+
+    return (
+        <div class="header">
+            <p class="header-text">Welcome to the Youtube Comments Analysis</p>
+        </div>
+    );
+}
+
+export default Header;
